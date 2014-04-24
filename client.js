7f75950db2beb4162269a6243b8d2a27c62cb5c8
Joe Henke <jdhenke@users.noreply.github.com>
2014-04-24 13:34:14 -0400

    Update client.js

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ page.onConsoleMessage = function(msg, lineNum, sourceId) {
   console.log('console message: ' + msg);
 };
 
-// load page, wait 0.5s for main/worker back and forth, exit
+// load page, wait 0.5s for main/worker back and forth to complete, exit.
 page.open("http://localhost:8000/", function() {
   setTimeout(function() {
     phantom.exit();
